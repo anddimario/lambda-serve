8af97c4553bfd28b42fcfc14a01be76a01033f9b
Andrea Di Mario <anddimario@gmail.com>
2017-12-09 17:06:54 +0100

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+Serve AWS Lambdas offline in you host
+
+### Features
+- pure nodejs, no modules
+- run multiple lambdas as a local app
+
+### Example usage
+- create a directory for all microservices (example: /myhome/myapp/microservices)
+- in this directory, for this example, clone: `git clone git@github.com:anddimario/aws-lambda-user-microservice.git user` (/myhome/myapp/microservices/user)
+- clone this repo (path is not important)
+- create an env.js, example:
+```
+module.exports = {
+  PORT: 3000,
+  lambdasDir: '/myhome/myapp/microservices',
+  processEnv: {
+    DYNAMO_REGION: 'eu-west-1',
+    DYNAMO_ENDPOINT: 'http://localhost:8000',
+    JWT_SECRET: 'mygreatsecret'
+  }
+}
+```
+- run: `node server.js`
+- registration curl:
+```
+curl -d"type=register&email=prova@example.com&password=testpw&fullname=prova" http://localhost:3000/user
+```
+- login curl:
+```
+curl -d"type=login&email=prova@example.com&password=testw" http://localhost:3000/user
+```
---
+++ server.js
@@ -0,0 +1,74 @@
+const querystring = require('querystring');
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const exec = require('child_process').exec;
+const env = require('./env');
+
+// set env variables
+for (const varEnv in env.processEnv) {
+  process.env[varEnv] = env.processEnv[varEnv];
+}
+
+const requestHandler = (req, res) => {
+
+  const parsedUrl = url.parse(req.url, true);
+  const service = parsedUrl.pathname.split('/')[1];
+  fs.exists(`${env.lambdasDir}/${service}/index.js`, (exists) => {
+    if (exists) {
+      const body = [];
+      req.on('error', function (err) {
+        console.log(err);
+      }).on('data', function (chunk) {
+        body.push(chunk);
+      }).on('end', function () {
+        let stringBody = Buffer.concat(body).toString();
+        if (stringBody) {
+          stringBody = querystring.parse(stringBody);
+
+        }
+
+        const event = {
+          httpMethod: req.method,
+          body: JSON.stringify(stringBody),
+          queryStringParameters: parsedUrl.query
+        };
+        fs.writeFile(`${env.lambdasDir}/${service}/testEvent.json`, JSON.stringify(event), (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(`Event not writed ${err}`);
+          } else {
+            const lambda = require(`${env.lambdasDir}/${service}/index`);
+            lambda.handler(event, {}, (err, result) => {
+              if (err) {
+                res.statusCode = 500;
+                res.end(`Event not writed ${err}`);
+              } else {
+                res.statusCode = result.statusCode;
+                for (const header in result.headers) {
+                  res.setHeader(header, result.headers[header]);
+                }
+                res.end(result.body);
+              }
+            });
+          }
+
+        });
+      });
+
+    } else {
+      res.statusCode = 500;
+      res.end('Service not exists');
+    }
+  });
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(env.PORT, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(`Started on ${env.PORT}`)
+})
+
